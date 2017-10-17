6d9bc267f57e5d6d6a928ecbd5b3a263bfc03b78
Bhanu <bhanu499@gmail.com>
2017-10-17 19:26:05 +0530

    First COmmit

---
+++ TwitterAPITest/ITweet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterAPITest
+{
+    interface ITweet
+    {
+        void GetTweetsAsync(string searchText);
+    }
+}
---
+++ TwitterAPITest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TwitterAPITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tweet t = new Tweet();
+
+
+            t.GetTweetsAsync("datascience");
+            Console.ReadKey();
+            
+        }
+    }
+}
